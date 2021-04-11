33cc9173e94a608ddb8dcebb79eed661340bc886
Jarrete Barnett <jarretebarnett@gmail.com>
2021-04-11 17:35:05 -0400

    Updates to SQL files and server.js calls.

---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+USE employeetracker_db;
+
+INSERT INTO department(name)
+VALUE ("Human Resources Management");
+
+INSERT INTO department(name)
+VALUE ("Production");
+
+INSERT INTO department(name)
+VALUE ("Research & Development");
+
+INSERT INTO department(name)
+VALUE ("Accounting & Finance");
+
+INSERT INTO department(name)
+VALUE ("Sales & Marketing");
--- server.js
+++ server.js
@@ -1,1 +1,4 @@
+const mysql = require("mysql");
+const express = require("express");
 const inquirer = require("inquirer");
+// const table = require("console.table");
