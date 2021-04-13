8583c32f5cb4f2273b9a91d17b2d1ba3a19ded7a
Jarrete Barnett <jarretebarnett@gmail.com>
2021-04-13 10:45:03 -0400

    Progress in server.js file.

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const mysql = require("mysql");
 const express = require("express");
 const inquirer = require("inquirer");
-// const table = require("console.table");
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: employeetracker_db
+});
+
+const addDepartment = () => {
+
+}
+
+const addRole = () => {
+    
+}
+
+const addEmployee = () => {
+    
+}
+
+const showDepartments = () => {
+
+}
+
+const showRoles = () => {
+    
+}
+
+const showEmployees = () => {
+    
+}
+
+const editRole = () => {
+    
+}
